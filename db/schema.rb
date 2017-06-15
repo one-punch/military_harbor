9a2dc7fd009101535927e6459a3efdb27cd372af
eamon <eamonding@gmail.com>
2017-06-16 03:08:47 +0800

    update user model

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20170615165325) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
