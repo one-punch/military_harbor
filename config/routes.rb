6c9afd260bf77b343eb8b08fe7b76d0231374490
eamon <eamonding@gmail.com>
2017-06-16 03:56:04 +0800

    add user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
 
+  get '/signup', to: 'users#new'
+
+  resources :users
+
   namespace :admin do
     root to: 'home#index', as: 'root'
   end
