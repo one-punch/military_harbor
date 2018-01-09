5df1000e327e5ca8f7b8a224aff49ec2908d4c27
eamon <eamonding@gmail.com>
2018-01-10 01:10:22 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,17 @@ class ApplicationController < ActionController::Base
     current_cart.cart_items
   end
 
+  private
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 end
