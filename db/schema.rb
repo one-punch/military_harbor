9f036f12f7dbb266857f54c5e1f61f1b6332265c
eamon <eamonding@gmail.com>
2018-03-20 01:15:43 +0800

    update user login

--- db/schema.rb
+++ db/schema.rb
@@ -102,6 +102,7 @@ ActiveRecord::Schema.define(version: 20180119160157) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.string "remember_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
