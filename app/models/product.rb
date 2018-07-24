17ae031b1d11b9f3d8b15a24cb4ec3e4c683370d
eamon <eamonding@gmail.com>
2018-07-25 00:25:31 +0800

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,12 +8,12 @@ class Product < PrimeryProduct
   scope :root, -> { where(parent_id: nil) }
 
   def self.import file
-    accessible_attributes = %w(name sku price description category_id product_detail_id)
+    accessible_attributes = %w(name sku price weight description category_id product_detail_id)
     CSV.foreach(file.path, headers: true) do |row|
       if row["property"]
         product = find_by(sku: row["sku"])
         if product
-          property_params = row["property"].split(";").map {|x| ["key", "value"].zip(x.split(":")).to_h}
+          property_params = row["property"].split(";").map {|x| ["key", "value"].zip(x.split(":").map(&:strip)).to_h}
           sku = property_params.map{|x| x.values}.sort.to_h.values.unshift(row["sku"]).join('-')
           variant = Variant.find_by(sku: sku) || product.variants.new
           variant.attributes = row.to_hash.slice(*accessible_attributes)
@@ -25,7 +25,7 @@ class Product < PrimeryProduct
           row["images"].split(";").each {|x| variant.pictures.create(name: x) } if row["images"].present?
         end
       else
-        product = find_by(sku: row["sku"])
+        product = find_by(sku: row["sku"]) || new
         product.attributes = row.to_hash.slice(*accessible_attributes)
         product.save!
         row["images"].split(";").each {|x| product.pictures.create(name: x) } if row["images"].present?
