3f8567ab72d744a90591497f8c1cfbb538f96dbb
eamon <eamonding@gmail.com>
2018-10-29 00:25:19 +0800

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @categories = Category.roots.select { |category| category.pictures.any? }
+    @categories = Category.roots.actived.select { |category| category.pictures.any? }
     @products = Product.limit 12
   end
 end
