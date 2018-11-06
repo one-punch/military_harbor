92af855a43f6008fdd51ee4081dec88b9d86a4e1
eamon <eamonding@gmail.com>
2018-11-06 19:43:01 +0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,9 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address:              ENV["MAILER_ADDRESS"],
+    address:              'smtp.exmail.qq.com',
     port:                 25,
     user_name:            ENV["MAILER_USERNAME"],
     password:             ENV["MAILER_PASSWORD"],
