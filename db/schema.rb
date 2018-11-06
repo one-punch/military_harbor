f6005fb404c05963e0ef4da0ff76039c64d872e9
eamon <eamonding@gmail.com>
2018-11-06 16:47:11 +0800

    user password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181106050057) do
+ActiveRecord::Schema.define(version: 20181106071917) do
 
   create_table "attachments", force: :cascade do |t|
     t.integer "source_id", null: false
@@ -155,6 +155,8 @@ ActiveRecord::Schema.define(version: 20181106050057) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
