15241fa69e17fdffd0d4361b8294ad45f0f1af7d
eamon <eamonding@gmail.com>
2018-11-06 18:32:14 +0800

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
+    address:              ENV["MAILER_ADDRESS"],
+    port:                 ENV["MAILER_PORT",
     user_name:            ENV["MAILER_USERNAME"],
     password:             ENV["MAILER_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    authentication:       'login',
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
