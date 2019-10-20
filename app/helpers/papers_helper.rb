945e64c5a1c818dad6f647aa2500ef32478db3dc
scalaview <chailink100@gmail.com>
2019-10-20 21:40:05 +0800

    add show

--- app/helpers/papers_helper.rb
+++ app/helpers/papers_helper.rb
@@ -5,5 +5,13 @@ module PapersHelper
     current_user.admin? || UserPaperRecord.where(user_id: current_user.id, paper_id: paper_id).last&.not_expired?
   end
 
+  def can_read?(product)
+    if product.is_a? Variant
+       can_view?(product.paper.id)
+    else
+      false
+    end
+  end
+
 
 end
