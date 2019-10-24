6070b528c1b3cef48e2c1ec1d07a67a052f3147c
scalaview <chailink100@gmail.com>
2019-10-24 21:57:59 +0800

    rm pry

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,6 +1,5 @@
 class AdminConstraint
   def matches?(request)
-    binding.pry
     return false unless request.session[:user_id]
     user = User.find request.session[:user_id]
     user && user.admin?
