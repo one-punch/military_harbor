76722f5758f96184d7c8d94897125a1376fcf4ad
scalaview <chailink100@gmail.com>
2019-10-25 16:20:27 +0800

    init product data

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
+  belongs_to :subject
+  belongs_to :grade
+  has_many :materials, primary_key: "proto_id", foreign_key: "proto_course_id"
+
   serialize :attrs, JSON
 end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ApplicationRecord
+  belongs_to :material, primary_key: "proto_id", foreign_key: "proto_material_id"
 
   def student_version
     "#{name} (#{I18n.t('paper.student')})"
