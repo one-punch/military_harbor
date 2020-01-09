13b998e549e10f402382f43ecfeacf71afbd0d48
scalaview <chailink100@gmail.com>
2020-01-09 17:16:29 +0800

    menu finish

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,7 +1,7 @@
 class ChaptersController < ApplicationController
 
   def show
-    @knowledge = Paper.kno_ins.where(proto_id: "8aac49074ebb3d6e014ebe8457fb027f").take
+    @knowledge = Paper.kno_ins.where(proto_id: "8aac49074ebb3d6e014ecd6224950397").take
     @titles = @knowledge.exam_paper_elements.order("CAST(number AS UNSIGNED)").select{|e| e.is_h1? || e.is_h2?}
   end
 
