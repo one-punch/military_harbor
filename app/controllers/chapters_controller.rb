3ad0ba645662e2982593d0087f5e3af7e0aabeab
scalaview <chailink100@gmail.com>
2020-01-14 15:52:45 +0800

    fix order

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -2,7 +2,8 @@ class ChaptersController < ApplicationController
 
   def show
     @knowledge = Paper.kno_ins.where(proto_id: "8aac49074ebb3d6e014ecd6224950397").take
-    @titles = @knowledge.exam_paper_elements.order("CAST(number AS UNSIGNED)").select{|e| e.is_h1? || e.is_h2?}
+    @exam_paper_elements = @knowledge.exam_paper_elements.preload(:question).order("CAST(number AS UNSIGNED)")
+    @titles = @exam_paper_elements.select{|e| e.is_h1? || e.is_h2?}
     render layout:  'chapter'
   end
 
