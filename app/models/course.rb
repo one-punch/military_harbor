f32685ca9d1c2f9dcc8dfbd52c5435840629faa6
scalaview <chailink100@gmail.com>
2020-01-15 17:34:30 +0800

    add child

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,11 @@
 class Course < ApplicationRecord
   belongs_to :subject
   belongs_to :grade
-  has_many :materials, primary_key: "proto_id", foreign_key: "proto_course_id"
+  has_many :materials,  -> { where("materials.proto_parent_id" => nil) }, primary_key: "proto_id", foreign_key: "proto_course_id"
+  belongs_to :parent, primary_key: "proto_id", foreign_key: "proto_parent_id", class_name: "Course"
+  has_many :child, primary_key: "proto_id", foreign_key: "proto_parent_id", class_name: "Course"
+
+  scope :base, -> { where("courses.proto_parent_id" => nil) }
 
   serialize :attrs, JSON
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,6 +1,8 @@
 class Material < ApplicationRecord
   belongs_to :course, primary_key: "proto_id", foreign_key: "proto_course_id"
   has_many :papers, primary_key: "proto_id", foreign_key: "proto_material_id"
+  belongs_to :parent, primary_key: "proto_id", foreign_key: "proto_parent_id", class_name: "Material"
+  has_many :child, primary_key: "proto_id", foreign_key: "proto_parent_id", class_name: "Material"
 
   serialize :attrs, JSON
 end
